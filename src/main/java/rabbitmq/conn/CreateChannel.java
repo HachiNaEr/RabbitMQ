5b741d2c3e9c5308e82647026ee54f47426abbd7
liuyongjian <liuyongjian@sqhtech.com>
2017-08-08 17:15:22 +0800

    创建多个channel

---
+++ src/main/java/rabbitmq/conn/CreateChannel.java
@@ -0,0 +1,44 @@
+package rabbitmq.conn;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+
+public class CreateChannel {
+	Logger logger = LoggerFactory.getLogger(getClass());
+	
+	public Map<Integer, Channel> channelMap = new HashMap<>();
+	
+	public void create(int count){
+		Connection connection = new TCPConn().conn();
+		if (count > connection.getChannelMax()) {
+			logger.debug("max channel : " + connection.getChannelMax());
+			return;
+		}
+		for (int index = 0; index < count; index++) {
+			try {
+				Channel channel = connection.createChannel();
+				if (channel != null) 
+					channelMap.put(channel.getChannelNumber(), channel);
+				else 
+					logger.debug("channel == null : fail allocate, count : " + count);
+			} catch (IOException exception) {
+				logger.debug(ExceptionUtils.getStackTrace(exception));
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		CreateChannel channels = new CreateChannel();
+		channels.create(2);
+		
+		System.err.println(channels.channelMap);
+	}
+}
